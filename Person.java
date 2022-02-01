8be02caad74de9b404e093207dfaa88259a3280f
Maciej <maciejfec1996@gmail.com>
2022-02-01 18:43:02 +0000

    added initialisers. added needed variables. added getters and setters for variables

--- Person.java
+++ Person.java
@@ -1,4 +1,52 @@
 package com.employeeManagementSystem;
 
 abstract class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private char gender;
+
+    public Person() {
+
+    }
+
+    public Person(String firstName, String lastName, int age, char gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return this.gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
 }
