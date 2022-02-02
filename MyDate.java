36ff750d7180b03564cc328a9e25e4cbf1818f0a
Maciej <maciejfec1996@gmail.com>
2022-02-02 17:25:30 +0000

    added necessary variables. added getters and setters for the variables.

--- MyDate.java
+++ MyDate.java
@@ -1,4 +1,53 @@
 package com.employeeManagementSystem;
 
 public class MyDate {
+
+    private int day;
+    private int month;
+    private int year;
+    private String date;
+
+    public MyDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.date = this.day + "/" + this.month + "/" + this.year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return this.getDay() + "/" + this.getMonth() + "/" + this.getYear();
+    }
 }
