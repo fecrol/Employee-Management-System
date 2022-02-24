be55cdb41c545faf28651fe68929c21d80658f3c
Maciej <maciejfec1996@gmail.com>
2022-02-24 16:42:50 +0000

    added empty methods: validateForm(), isNumber(), and checkIdExists().

--- FormValidation.java
+++ FormValidation.java
@@ -1,4 +1,12 @@
 package com.employeeManagementSystem;
 
 public interface FormValidation {
+
+    public abstract boolean validateForm(String id, String firstName, String lastName, String age, String gender);
+
+    public abstract boolean validateForm(String id, MyDate dateFrom, MyDate dateTo);
+
+    public abstract boolean isNumber(String input);
+
+    public abstract boolean checkIdExists(int id);
 }
