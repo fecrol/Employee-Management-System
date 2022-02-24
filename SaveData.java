82909e31451cac8f2a8e277cbfeb068974035885
Maciej <maciejfec1996@gmail.com>
2022-02-24 16:38:39 +0000

    added code to the SaveData interface for saving data to a text file.

--- SaveData.java
+++ SaveData.java
@@ -1,4 +1,29 @@
 package com.employeeManagementSystem;
 
+import javax.swing.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 public interface SaveData {
+
+    default void saveData(String pathname, ArrayList arrayList) {
+        File file = new File(pathname);
+
+        try {
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            for (Object object : arrayList) {
+                bw.write(object.toString());
+                bw.newLine();
+            }
+
+            bw.close();
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error saving file", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
