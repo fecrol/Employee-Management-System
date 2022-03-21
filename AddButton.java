e0160fb2f1e02c5d9cd4f5d1319efa7163e4a683
Maciej <maciejfec1996@gmail.com>
2022-03-21 11:50:48 +0000

    add button to add employee to employee list or holiday request to holiday requests list

--- AddButton.java
+++ AddButton.java
@@ -1,4 +1,28 @@
 package com.employeeManagementSystem;
 
-public class AddButton {
+public class AddButton extends Button {
+
+    private Employees employees;
+
+    public AddButton(String text, Employees employees) {
+        super(text);
+        this.employees = employees;
+    }
+
+    public void addEmployee(Table employeesTable, int id, String department, String firstName, String lastName, int age, char gender, int holidaysRemaining, int holidaysRequested) {
+        Employee employee = new Employee(id, department, firstName, lastName, age, gender, holidaysRemaining, holidaysRequested);
+
+        int index = this.employees.getEmployees().size();
+        for (int i=this.employees.getEmployees().size() - 1; i>=0; i--) {
+            if (id > this.employees.getEmployees().get(i).getId()) {
+                break;
+            }
+            index--;
+        }
+
+        Object[] data = {String.valueOf(id), department, firstName, lastName, String.valueOf(age), String.valueOf(gender), String.valueOf(holidaysRemaining), String.valueOf(holidaysRequested)};
+
+        employeesTable.insertRow(index, data);
+        this.employees.addEmployee(index, employee);
+    }
 }
