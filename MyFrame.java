a28290e66dca518a74ab76a8d6f73f0da83dc2b1
Maciej <maciejfec1996@gmail.com>
2022-03-21 11:45:33 +0000

    set up window for the GUI

--- MyFrame.java
+++ MyFrame.java
@@ -1,4 +1,20 @@
 package com.employeeManagementSystem;
 
-public class MyFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+
+    private String title;
+
+    public MyFrame() {
+        this.title = "Workforce Management";
+
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setResizable(false);
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setTitle(this.title);
+    }
 }
